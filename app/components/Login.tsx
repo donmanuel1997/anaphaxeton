ac13936017967ec8894a0bcc2798b834d1bf9e31
Adelakun Emmanuel <donmanuel1997@gmail.com>
2023-03-11 16:58:38 +0000

    first commit

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -10,65 +10,66 @@ type Props = {}
 
 export default function Login({}: Props) {
   return (
-    <div className='lg:h-screen w-screen flex flex-col bg-gray-800'>
+    <div className='lg:h-screen w-screen flex-1 flex flex-col bg-gray-800'>
     <Head />
-    <div className='flex flex-col my-2 justify-center items-center'>
-      <div className=' animate-pulse text-2xl font-bold rounded-full text-white tracking-widest p-1 bg-gray-900'>
-        <img src='./openai.jpg' className='w-12 h-12 rounded-full' />
-      </div>
-      <div className='text-2xl font-bold text-white tracking-widest'>
+    <div className='flex-1 flex-col my-2 justify-center items-center'>
+      
+      <div className='flex-col justify-center items-center'>
+     
+      <div className='flex justify-center items-center text-2xl font-bold text-white tracking-widest'>
         ANAPHAXETON
       </div>
       <div className='m-8'>
-        <p className='text-white text-xs'>
+        <p className='text-white text-xs text-center'>
         Creating safe artificial general intelligence that benefits all of humanity.        
-        We{"'"}ve trained a model called "ANAPHAXETON" ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT to answer follow-up questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.
+        <br></br>We{"'"}ve trained a model called "ANAPHAXETON" ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT to answer follow-up questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.
         
         </p>
       </div>
-      <div className=''>
-        <button onClick={() => signIn("google")} className=' animate-bounce flex justify-center items-center bg-gray-700  w-[150px] px-5 py-2 rounded-lg text-xs text-white'>
+      <div className='flex justify-center items-center '>
+        <button onClick={() => signIn("google")} className=' border border-white/40 animate-bounce flex justify-center items-center bg-gray-700  w-[150px] px-5 py-2 rounded-lg text-xs text-white'>
           <img src='./g.png'  className='w-6 h-6 rounded-full mr-3' />
           Sign In
         </button>
       </div>
 
-      <div className='flex flex-col lg:flex-row  mb-3  rounded-lg space-x-2 text-center'>
+      </div>
+      <div className='flex-1 lg:flex flex-col lg:flex-row  mb-3  rounded-lg text-center'>
         <div className='mb-5 lg:mb-0 flex-1  p-3 m-3 bg-white'>
-          <div className='flex flex-col items-center justify-center mb-5'>
+        <div className='flex flex-1  flex-col items-center justify-center mb-5'>
           <SunIcon className="h-6 w-6  text-gray-800"/>
             <h2>Example</h2>
           </div>
-          <div className=' space-y-2'>
-            <p className='hText'>Explain something to me</p>
-            <p className='hText'>What is the difference between a dog and a cat?</p>
-            <p className='hText'>What is the color of the sun?</p>
+          <div className='flex-col flex-1 space-y-2'>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>Explain something to me</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>What is the difference between a dog and a cat?</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>What is the color of the sun?</p>
           </div>
         </div>
 
 
-        <div  className='mb-5 lg:mb-0  p-3 m-3 bg-white'>
-          <div className='flex flex-col items-center justify-center mb-5'>
+        <div  className='flex-1  mb-5 lg:mb-0  p-3 m-3 bg-white'>
+          <div className='flex flex-1  flex-col items-center justify-center mb-5'>
           <BoltIcon className="h-6 w-6 text-gray-800"/>
             <h2>Capabilities</h2>
           </div>
-          <div className=' space-y-2'>
-            <p className='hText'>Use any ChatGPT model</p>
-            <p className='hText'>Fully functional database.</p>
-            <p className='hText'>Hot Toast notifications when chatGPT is thinking! </p>
+          <div className=' flex-col flex-1  space-y-2'>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>Use any ChatGPT model</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>Fully functional database.</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>Hot Toast notifications when chatGPT is thinking! </p>
           </div>
         </div>
 
 
-        <div  className='mb-5 lg:mb-0  p-3 m-3 bg-white'>
-          <div className='flex flex-col items-center justify-center mb-5'>
+        <div  className='flex-1 mb-5 lg:mb-0  p-3 m-3 bg-white'>
+          <div className='flex flex-1  flex-col items-center justify-center mb-5'>
           <ExclamationTriangleIcon className="h-6 w-6  text-gray-800"/>
             <h2>Limitations</h2>
           </div>
-          <div className=' space-y-2'>
-            <p className='hText'>May occasionally generate incorrect information</p>
-            <p className='hText'>May occationally produce harmful instructions or biased contents</p>
-            <p className='hText'>Limited knowledge of the world and events after 2021</p>
+          <div className=' flex-col flex-1  space-y-2'>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>May occasionally generate incorrect information</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>May occationally produce harmful instructions or biased contents</p>
+            <p className='bg-gray-700 text-white text-xs p-3 rounded-lg flex-1'>Limited knowledge of the world and events after 2021</p>
           </div>
         </div>
       </div>
